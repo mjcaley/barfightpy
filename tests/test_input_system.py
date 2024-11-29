66d007ece41da02228029370046b365c181814d9
Michael Caley <mjcaley@darkarctic.com>
2024-11-29 13:31:06 -0500

    Fixed input system tests

--- tests/test_input_system.py
+++ tests/test_input_system.py
@@ -2,6 +2,7 @@ import pytest
 from pyglet.math import Vec2
 from pyglet.window import key
 from pyglet.window.key import KeyStateHandler
+from pyglet.window.mouse import MouseStateHandler
 
 from barfight import ecs, events
 from barfight.components import Actor
@@ -17,14 +18,19 @@ from barfight.systems import InputSystem
         (key.D, Vec2(1, 0)),
     ],
 )
-def test_input_sets_direction(ecs_world, test_input, expected):
-    handler = KeyStateHandler()
-    ecs.add_system(InputSystem(handler))
+def test_input_sets_direction(mocker, ecs_world, test_input, expected):
+    key_handler = mocker.MagicMock()
+    key_handler.__getitem__.side_effect = lambda k: k == test_input
 
     player = Actor(0)
     ecs.create_entity(player)
+    
+    def player_direction_handler(direction: Vec2):
+        player.direction = direction
 
-    handler.on_key_press(test_input, 0)
+    ecs.add_system(InputSystem(key_handler, MouseStateHandler()))
+    ecs.set_handler(events.PLAYER_DIRECTION_EVENT, player_direction_handler)
+
     ecs.update(0.0)
 
     assert expected == player.direction
@@ -39,7 +45,7 @@ def test_input_on_key_down(ecs_world):
 
     ecs.set_handler(events.PLAYER_ATTACK_EVENT, on_player_attack)
 
-    input_system = InputSystem(KeyStateHandler())
+    input_system = InputSystem(KeyStateHandler(), MouseStateHandler())
     ecs.add_system(input_system)
 
     input_system.on_key_down(key.N, 0)
