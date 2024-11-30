dd91f622e251507153804c3cfad661322a6e6e06
Michael Caley <mjcaley@darkarctic.com>
2024-11-29 21:46:44 -0500

    Ray has correct boundary

--- barfight/physics/raycast.py
+++ barfight/physics/raycast.py
@@ -16,13 +16,12 @@ class RayIntersection:
 class Ray:
     def __init__(self, origin: Vec2 = None, direction: Vec2 = None, max_distance: float = 1.0):
         self.origin = origin or Vec2()
-        self._direction = direction or Vec2()
-        self._direction.normalize()
+        self._direction = (direction or Vec2()).normalize()
         self.max_distance = max_distance
 
     def boundary(self) -> Rectangle:
         ray_start = self.origin
-        ray_end = (self.origin + self.direction).from_magnitude(self.max_distance)
+        ray_end = self.direction.from_magnitude(self.max_distance) + self.origin
 
         return Rectangle(Vec2(min(ray_start.x, ray_end.x), min(ray_start.y, ray_end.y)), Vec2(max(ray_start.x, ray_end.x), max(ray_start.y, ray_end.y)))
 
