a66c4f30724c085f72e69a713ba8f9d6f9c74b9a
Michael Caley <mjcaley@darkarctic.com>
2025-03-25 14:50:10 -0400

    Trying alternative GJK/EPA implementation

---
+++ barfight/physics/primitives/gjk2.py
@@ -0,0 +1,110 @@
+"""Implementation based off https://observablehq.com/@esperanc/2d-gjk-and-epa-algorithms"""
+
+from dataclasses import dataclass
+from math import inf
+from pyglet.math import Vec2
+
+
+def triple_product(a: Vec2, b: Vec2, c: Vec2) -> Vec2:
+    z = a.x * b.y - a.y * b.x
+    
+    return Vec2(-c.y * z, c.x * z)
+
+
+def support(points: list[Vec2], direction: Vec2) -> Vec2:
+    max_dot = -inf
+    best = None
+    for point in points:
+        dot = point.dot(direction)
+        if dot > max_dot:
+            max_dot = dot
+            best = point
+
+    return best
+
+
+def support2(points1: Vec2, points2: Vec2, direction: Vec2) -> Vec2:
+    return support(points1, direction) - support(points2, -direction)
+
+
+def gjk(points1: list[Vec2], points2: list[Vec2]) -> list[Vec2] | None:
+    # First point
+    a = support2(points1, points2, Vec2(1, 1))
+
+    # First direction
+    v = -a
+
+    # Second point
+    b = support2(points1, points2, v)
+
+    # Second direction
+    ab = b - a
+    v = triple_product(ab, -a, ab)
+
+    while True:
+        # Third point
+        c = support2(points1, points2, v)
+        if c.dot(v) <= 0.0:
+            # Not past the origin
+            return None
+        
+        c0 = -c
+        cb = b - c
+        ca = a - c
+
+        cb_perp = triple_product(ca, cb, cb)
+        ca_perp = triple_product(cb, ca, ca)
+
+        if ca_perp.dot(c0) > 0.0:
+            b = c
+            v = ca_perp
+        elif cb_perp.dot(c0) > 0.0:
+            a = c
+            v = cb_perp
+        else:
+            return [a, b, c]
+
+
+@dataclass
+class Edge:
+    distance: float
+    index: int
+    first: Vec2
+    second: Vec2
+    normal: Vec2
+
+
+def closest_edge(polytope: list[Vec2]) -> Edge:
+    npts = len(polytope)
+    dmin = inf
+    closest = None
+    for i in range(npts):
+        p, q = polytope[i], polytope[(i + 1) % npts]
+        e = q - p
+        n = triple_product(e, p, e).normalize()
+        dist = n.dot(p)
+        if dist < dmin:
+            dmin = dist
+            closest = Edge(dist, i, p, q, n)
+
+    return closest
+
+
+@dataclass
+class Intersection:
+    first: Vec2
+    second: Vec2
+    distance: float
+    normal: Vec2
+
+
+
+def epa(points1: list[Vec2], points2: list[Vec2], a: Vec2, b: Vec2, c: Vec2):
+    polytope = [a, b, c]
+
+    while True:
+        edge = closest_edge(polytope)
+        r = support2(points1, points2, edge.normal)
+        if abs(edge.normal.dot(r) - edge.distance < 0.0001):
+            return Intersection(edge.first, edge.second, edge.distance, edge.normal)
+        polytope.insert(edge.index + 1, r)
---
+++ tests/test_physics_primitives_gjk2.py
@@ -0,0 +1,149 @@
+from math import radians
+
+import pytest
+from pyglet.math import Vec2
+
+from barfight.physics.primitives import (
+    Circle,
+    OrientedRectangle,
+    Point,
+    Polygon,
+    Rectangle,
+)
+from barfight.physics.primitives.gjk2 import (
+    gjk, epa
+)
+
+
+@pytest.mark.parametrize(
+    "test_shape1,test_shape2",
+    [
+        (
+            Rectangle(Vec2(0, 0), Vec2(1, 1)),
+            Rectangle(Vec2(0.5, 0), Vec2(1, 1)),
+        ),  # overlapping
+        (
+            Rectangle(Vec2(0, 0), Vec2(1.0001, 1)),
+            Rectangle(Vec2(1, 1), Vec2(1, 1)),
+        ),  # edge
+        (
+            OrientedRectangle(Vec2(0, 0), Vec2(5, 5), 45),
+            OrientedRectangle(Vec2(0.5, 0), Vec2(5, 5), 45),
+        ),
+        (
+            Polygon([Vec2(-1, -1), Vec2(0, 1), Vec2(1, -1)]),
+            Polygon([Vec2(-1, 1), Vec2(1, 1), Vec2(0, -1)]),
+        ),
+    ],
+)
+def test_colliding_intersection(test_shape1, test_shape2):
+    result = gjk([v for v in test_shape1.vertices()], [v for v in test_shape2.vertices()])
+
+    assert None is not result
+
+
+@pytest.mark.parametrize(
+    "test_shape1,test_shape2",
+    [
+        (Rectangle(Vec2(0, 0), Vec2(1, 1)), Rectangle(Vec2(0, 2), Vec2(1, 1))),
+        # (Circle(Vec2(0, 0), 1), Circle(Vec2(10, 0), 1)),
+        (
+            OrientedRectangle(Vec2(0, 0), Vec2(5, 5), 45),
+            OrientedRectangle(Vec2(10, 10), Vec2(5, 5), 45),
+        ),
+        (
+            Polygon([Vec2(-1, -1), Vec2(0, 1), Vec2(1, -1)]),
+            Polygon([Vec2(10, 10), Vec2(10.5, 11), Vec2(11, 10)]),
+        ),
+        # (Point(Vec2(0, 0)), Point(Vec2(1, 0))),
+    ],
+)
+def test_colliding_no_intersection(test_shape1, test_shape2):
+    result = gjk([v for v in test_shape1.vertices()], [v for v in test_shape2.vertices()])
+
+    assert None is result
+
+
+@pytest.mark.parametrize(
+    "test_shape1,test_shape2,expected_normal,expected_depth",
+    [
+        (
+            Rectangle(Vec2(0, 0), Vec2(1, 1)),
+            Rectangle(Vec2(0.5, 0), Vec2(1, 1)),
+            Vec2(1, 0),
+            0.5,
+        ),
+        (
+            Circle(Vec2(0, 0), 0.5),
+            Circle(Vec2(0.5, 0), 0.5),
+            Vec2(1, 0),
+            0.5,
+        ),  # overlapping
+        # (Circle(Vec2(0, 0), 1), Circle(Vec2(2, 0), 1), Vec2(1, 0), 0),  # edge, doesn't work, GJK doesn't see collision
+        (
+            OrientedRectangle(Vec2(0, 0), Vec2(5, 5), radians(45)),
+            OrientedRectangle(Vec2(x=5, y=5), Vec2(5, 5), radians(45)),
+            Vec2(1, 1).normalize(),
+            2.9289321881345254,
+        ),
+        # Shapes from dyn4j article: https://dyn4j.org/2010/05/epa-expanding-polytope-algorithm/
+        (
+            Polygon([Vec2(4, 5), Vec2(4, 11), Vec2(9, 9)]),
+            Polygon([Vec2(7, 3), Vec2(5, 7), Vec2(12, 7), Vec2(10, 2)]),
+            Vec2(0.62, -0.78),
+            0.93,
+        ),
+    ],
+)
+def test_penetration(test_shape1, test_shape2, expected_normal, expected_depth):
+    collision = colliding(test_shape1, test_shape2)
+    result = penetration(collision)
+
+    assert pytest.approx(expected_normal.x, rel=1e-1, abs=1e-1) == result.normal.x
+    assert pytest.approx(expected_normal.y, rel=1e-1, abs=1e-1) == result.normal.y
+    assert pytest.approx(expected_depth, rel=1e-2, abs=1e-2) == result.distance
+
+
+def test_closest_edge():
+    p = [Vec2(-1, -1), Vec2(-1, 1), Vec2(3, 1), Vec2(3, -1)]
+    edge = closest_edge(p)
+
+    assert edge is not None
+
+
+def test_gjk_again():
+    points1 = list(Polygon([Vec2(4, 5), Vec2(4, 11), Vec2(9, 9)]).vertices())
+    points2 = list(Polygon([Vec2(7, 3), Vec2(5, 7), Vec2(12, 7), Vec2(10, 2)]).vertices())
+    a, b, c = [Vec2(4, 2), Vec2(-8, -2), Vec2(-1, -2)]
+
+    result = epa(points1, points2, a, b, c)
+
+    assert result is not None
+
+
+@pytest.mark.xfail(reason="GJK detects collision, but EPA doesn't find a depth")
+def test_epa_no_depth():
+    r1 = list(Rectangle(origin=Vec2(x=250.0, y=141.90910799981793), size=Vec2(x=100, y=100)).vertices())
+    r2 = list(Rectangle(origin=Vec2(x=350.0, y=150.0), size=Vec2(x=100, y=100)).vertices())
+
+    a, b, c = gjk(r1, r2)
+    assert a is not None
+    assert b is not None
+    assert c is not None
+
+    p = epa(r1, r2, a, b, c)
+    assert p.distance != 0.0
+
+
+def test_epa_incorrect_penetration():
+    r1 = list(Rectangle(origin=Vec2(x=250.0001, y=160.00352400002157), size=Vec2(x=100, y=100)).vertices())
+    r2 = list(Rectangle(origin=Vec2(x=350.0, y=150.0), size=Vec2(x=100, y=100)).vertices())
+
+    a, b, c = gjk(r1, r2)
+    assert a is not None
+    assert b is not None
+    assert c is not None
+
+    p = epa(r1, r2, a, b, c)
+
+    assert p.distance < 25
